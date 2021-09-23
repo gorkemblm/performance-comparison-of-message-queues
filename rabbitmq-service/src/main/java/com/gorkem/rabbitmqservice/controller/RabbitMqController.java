82f2866b78a3ab7e90ce966b255d80a083558d24
Görkem Bilim <83100550+gorkemblm@users.noreply.github.com>
2021-09-23 18:08:14 +0300

    added-post-method-for-add-to-queue-w-multi-thread

--- rabbitmq-service/src/main/java/com/gorkem/rabbitmqservice/controller/RabbitMqController.java
+++ rabbitmq-service/src/main/java/com/gorkem/rabbitmqservice/controller/RabbitMqController.java
@@ -1,9 +1,26 @@
 package com.gorkem.rabbitmqservice.controller;
 
+import com.gorkem.rabbitmqservice.model.User;
+import com.gorkem.rabbitmqservice.service.RabbitMqService;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/v1/rabbitmq")
 public class RabbitMqController {
+
+    private final RabbitMqService rabbitMqService;
+
+    public RabbitMqController(RabbitMqService rabbitMqService) {
+        this.rabbitMqService = rabbitMqService;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/add-to-queue")
+    public void addToQueue(@RequestBody User user) {
+        for (int i = 0 ; i < 8 ; i++) {
+            this.rabbitMqService.addToQueue(user);
+        }
+    }
 }
