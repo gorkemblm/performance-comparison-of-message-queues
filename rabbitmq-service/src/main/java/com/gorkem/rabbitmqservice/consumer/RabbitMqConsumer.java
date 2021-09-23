e37892f599c3d82e70e2a872bb51abe6765b237d
Görkem Bilim <83100550+gorkemblm@users.noreply.github.com>
2021-09-24 02:39:57 +0300

    added-rabbitmq-consumer

---
+++ rabbitmq-service/src/main/java/com/gorkem/rabbitmqservice/consumer/RabbitMqConsumer.java
@@ -0,0 +1,25 @@
+package com.gorkem.rabbitmqservice.consumer;
+
+import com.gorkem.rabbitmqservice.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class RabbitMqConsumer {
+
+    public static long count = 1;
+
+    //@Async
+    @RabbitListener(queues = "sampleQueue", autoStartup = "true")
+    public void recievedMessage(User user) {
+        try {
+            log.info("GENERAL COUNT : {}. RECEIVER THREAD : {}. Received Message From sampleQueue : {}.", count, Thread.currentThread().getName(), user);
+            count++;
+        } catch (Exception e) {
+            log.error("ERROR");
+        }
+    }
+}
