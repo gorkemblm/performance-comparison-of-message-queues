ed95adbc9a49ce639ed80fa1a0722b0bb3a59193
Görkem Bilim <83100550+gorkemblm@users.noreply.github.com>
2021-09-23 18:06:13 +0300

    added-convert-logic&post-method-w-rest-template

--- management-service/src/main/java/com/gorkem/managementservice/service/UserService.java
+++ management-service/src/main/java/com/gorkem/managementservice/service/UserService.java
@@ -1,7 +1,14 @@
 package com.gorkem.managementservice.service;
 
+import com.gorkem.managementservice.model.User;
+import com.gorkem.managementservice.model.dto.UserConverter;
+import com.gorkem.managementservice.model.dto.UserDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    public User convertForAddToRabbitMqQueue(UserDto userDto) {
+        return UserConverter.convertToEntity(userDto);
+    }
 }
