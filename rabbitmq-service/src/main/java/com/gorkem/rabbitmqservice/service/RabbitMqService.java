e6c41ee7a78024a9852cb5fe5f5d38c8c0e6ba58
Görkem Bilim <83100550+gorkemblm@users.noreply.github.com>
2021-09-23 18:08:52 +0300

    Rabbitmq service (#5)
    * added-dto-converter&user-dto
    
    * added-convert-logic&post-method-w-rest-template
    
    * added-rabbitmq-config-to-properties
    
    * added-writing-to-queue-logic-w-multi-thread
    
    * added-post-method-for-add-to-queue-w-multi-thread

--- rabbitmq-service/src/main/java/com/gorkem/rabbitmqservice/service/RabbitMqService.java
+++ rabbitmq-service/src/main/java/com/gorkem/rabbitmqservice/service/RabbitMqService.java
@@ -1,7 +1,34 @@
 package com.gorkem.rabbitmqservice.service;
 
+import com.gorkem.rabbitmqservice.model.User;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RabbitMqService {
+
+    private final DirectExchange exchange;
+
+    @Value("sampleRoutingKey")
+    String routingKey;
+
+    @Value("sampleQueue")
+    String queueName;
+
+    private final AmqpTemplate template;
+
+    public RabbitMqService(DirectExchange exchange, AmqpTemplate template) {
+        this.exchange = exchange;
+        this.template = template;
+    }
+
+    @Async
+    public void addToQueue(User user) {
+        for (int i = 1; i <= 750; i++) {
+            template.convertAndSend(exchange.getName(), user);
+        }
+    }
 }
