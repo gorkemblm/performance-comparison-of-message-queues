11f4b59ff5477c1121668b8b39d7002035a23a4c
Görkem Bilim <83100550+gorkemblm@users.noreply.github.com>
2021-09-23 15:12:16 +0300

    added-management-service

---
+++ management-service/src/test/java/com/gorkem/managementservice/ManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gorkem.managementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
