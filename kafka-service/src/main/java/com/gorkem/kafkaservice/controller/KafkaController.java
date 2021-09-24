f4afe8423ab7994560daec5d00fc50c2c44106af
Görkem Bilim <83100550+gorkemblm@users.noreply.github.com>
2021-09-24 22:33:03 +0300

    added-log-system-for-kafka-service

--- kafka-service/src/main/java/com/gorkem/kafkaservice/controller/KafkaController.java
+++ kafka-service/src/main/java/com/gorkem/kafkaservice/controller/KafkaController.java
@@ -2,11 +2,13 @@ package com.gorkem.kafkaservice.controller;
 
 import com.gorkem.kafkaservice.model.User;
 import com.gorkem.kafkaservice.service.KafkaService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/v1/kafka")
 public class KafkaController {
@@ -19,8 +21,11 @@ public class KafkaController {
 
     @RequestMapping(method = RequestMethod.POST, path = "/add-to-line")
     public void addToStream(@RequestBody User user) {
-        for (int i = 0 ; i < 8 ; i++) {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 8; i++) {
             this.kafkaService.writeToKafkaStream(user);
         }
+        long stop = System.currentTimeMillis();
+        log.info("TOTAL TIME OF WRITE MESSAGES TO STREAM : {} MILLISECONDS", stop - start);
     }
 }
--- kafka-service/src/main/java/com/gorkem/kafkaservice/service/KafkaService.java
+++ kafka-service/src/main/java/com/gorkem/kafkaservice/service/KafkaService.java
@@ -1,10 +1,12 @@
 package com.gorkem.kafkaservice.service;
 
 import com.gorkem.kafkaservice.model.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class KafkaService {
 
@@ -17,8 +19,11 @@ public class KafkaService {
 
     @Async
     public void writeToKafkaStream(User user) {
-        for (int i = 0 ; i < 750 ; i++) {
+        long start = System.currentTimeMillis();
+        for (int i = 1; i <= 750; i++) {
             kafkaTemplate.send(topicNames[2], user.toString());
         }
+        long stop = System.currentTimeMillis();
+        log.info("THREAD NAME : {} TIME TAKEN FOR 750 MESSAGES IN KAFKA : {} MILLISECONDS", Thread.currentThread().getName(), stop - start);
     }
 }
