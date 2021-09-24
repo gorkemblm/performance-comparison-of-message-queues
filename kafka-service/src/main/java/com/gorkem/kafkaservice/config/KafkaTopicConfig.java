e101403e7a66bcbd3c1671bd92d6ee91e342ec07
Görkem Bilim <gorkemblm@gmail.com>
2021-09-24 18:13:58 +0300

    Kafka service scenario 2 (#7)
    * added-config-for-sync-one-topic-four-partition-four-consumer
    
    * added-topic-name-for-scenario-two
    
    * added-four-consumer-for-scenario-two

---
+++ kafka-service/src/main/java/com/gorkem/kafkaservice/config/KafkaTopicConfig.java
@@ -0,0 +1,20 @@
+package com.gorkem.kafkaservice.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.config.TopicConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    //SCENARIO 2 -> SYNC 1 TOPIC 4 PARTITION 4 CONSUMER CONFIG
+    @Bean
+    public NewTopic topicOfScenario() {
+        return TopicBuilder.name("sync-one-topic-four-partition")
+                .partitions(4)
+                .config(TopicConfig.COMPRESSION_TYPE_CONFIG, "zstd")
+                .build();
+    }
+}
--- kafka-service/src/main/java/com/gorkem/kafkaservice/consumer/KafkaConsumer.java
+++ kafka-service/src/main/java/com/gorkem/kafkaservice/consumer/KafkaConsumer.java
@@ -8,12 +8,61 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaConsumer {
     public static long generalCount = 1;
+    public static long consumerOneCount = 1;
+    public static long consumerTwoCount = 1;
+    public static long consumerThreeCount = 1;
+    public static long consumerFourCount = 1;
 
     //1 TOPIC 1 PARTITION 1 CONSUMER SCENARIO -> {1 TO 1 CONNECTION}
-    @KafkaListener(autoStartup = "true", topics = "sync-one-topic-one-partition", groupId = "scenario-1")
+    /*@KafkaListener(autoStartup = "true", topics = "sync-one-topic-one-partition", groupId = "scenario-1")
     public void recievedMessage(String message) {
         try {
             log.info("General Count : {}. CONSUMER : {}. Received Message From Stream: {}.", generalCount, "C1", message);
+            generalCount++;
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+    }*/
+
+    //1 TOPIC 4 PARTITION 4 CONSUMER SCENARIO -> {1 TO 1 CONNECTION}
+    @KafkaListener(autoStartup = "true", topics = "sync-one-topic-four-partition", groupId = "scenario-2")
+    public void recievedMessageFromMultiplePartitionListener1(String message) {
+        try {
+            log.info("General Count : {}.  Personal Count : {}. CONSUMER : {}. Received Message From Stream: {}.", generalCount, consumerOneCount, "C1", message);
+            consumerOneCount++;
+            generalCount++;
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    @KafkaListener(autoStartup = "true", topics = "sync-one-topic-four-partition", groupId = "scenario-2")
+    public void recievedMessageFromMultiplePartitionListener2(String message) {
+        try {
+            log.info("General Count : {}.  Personal Count : {}. CONSUMER : {}. Received Message From Stream: {}.", generalCount, consumerTwoCount, "C2", message);
+            consumerTwoCount++;
+            generalCount++;
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    @KafkaListener(autoStartup = "true", topics = "sync-one-topic-four-partition", groupId = "scenario-2")
+    public void recievedMessageFromMultiplePartitionListener3(String message) {
+        try {
+            log.info("General Count : {}.  Personal Count : {}. CONSUMER : {}. Received Message From Stream: {}.", generalCount, consumerThreeCount, "C3", message);
+            consumerThreeCount++;
+            generalCount++;
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    @KafkaListener(autoStartup = "true", topics = "sync-one-topic-four-partition", groupId = "scenario-2")
+    public void recievedMessageFromMultiplePartitionListener4(String message) {
+        try {
+            log.info("General Count : {}.  Personal Count : {}. CONSUMER : {}. Received Message From Stream: {}.", generalCount, consumerFourCount, "C4", message);
+            consumerFourCount++;
             generalCount++;
         } catch (Exception e) {
             log.error(e.getMessage());
